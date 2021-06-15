2a8259333949c8929ecc97922262ceecc9910017
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-06-15 16:01:39 -0500

    refactoring pet to include breed

--- domain/pets.domain.js
+++ domain/pets.domain.js
@@ -1,12 +1,13 @@
 // make classes
 class Pet {
-    constructor(name, species, sex, birthYear, birthMonth, imgSrc) {
+    constructor(name, species, sex, birthYear, birthMonth, imgSrc, breed) {
         this._name = name;
         this._species = species;
         this._sex = sex;
         this._birthYear = birthYear;
         this._birthMonth = birthMonth;
         this._imgSrc = imgSrc;
+        this._breed = breed;
     }
 }
 
